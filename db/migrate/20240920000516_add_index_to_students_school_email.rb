6f47334222d531aa5e5a510ad8caae6ae5d03ef5
Chase White <chase@Chases-MacBook-Pro.local>
2024-09-19 18:10:49 -0600

    completed first two user stories

---
+++ db/migrate/20240920000516_add_index_to_students_school_email.rb
@@ -0,0 +1,4 @@
+class AddIndexToStudentsSchoolEmail < ActiveRecord::Migration[7.1]
+  def change
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_19_231735) do
+ActiveRecord::Schema[7.1].define(version: 2024_09_20_000516) do
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
