1a9c6dddcf8f374c3d8e58e57f4dc2095ef3ca76
Chase White <chase@Chases-MacBook-Pro.local>
2024-09-19 18:50:43 -0600

    Added 8 tests in student test model

--- app/models/student.rb
+++ app/models/student.rb
@@ -1,10 +1,8 @@
 class Student < ApplicationRecord
-  email_regex = /^[A-Za-z0-9+_.-]+@[A-Za-z0-9.-]+$/
   validates :school_email, presence: true, uniqueness: true
   validates :first_name, presence: true 
   validates :last_name, presence: true
   validates :major, presence: true
-  validates :minor, presence: true
   validates :graduation_date, presence:true
   validates :school_email, format: { with: /\A[A-Za-z0-9+.-]+@[A-Za-z0-9.-]+\z/}
 
--- test/models/student_test.rb
+++ test/models/student_test.rb
@@ -1,7 +1,49 @@
 require "test_helper"
 
 class StudentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "first name cannot be blank" do
+    student = Student.new(first_name: " ", last_name: "White", school_email: "cwhite72@msudenver.edu", major: "Psychology", minor: "CS", graduation_date: 2024/12/13)
+    assert_not student.save
+    assert_includes student.errors[:first_name], "can't be blank"
+  end
+
+  test "can create a user" do
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: "cwhite72@msudenver.edu", major: "Psychology", minor: "CS", graduation_date: 2024/12/13)
+    assert student.save
+  end
+
+  test "last name cannot be blank" do 
+    student = Student.new(first_name: "Chase", last_name: " ", school_email: "cwhite72@msudenver.edu", major: "Psychology", minor: "CS", graduation_date: 2024/12/13)
+    assert_not student.save
+    assert_includes student.errors[:last_name], "can't be blank"
+  end
+
+  test "school email cannot be blank" do 
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: " ", major: "Psychology", minor: "CS", graduation_date: 2024/12/13)
+    assert_not student.save
+    assert_includes student.errors[:school_email], "can't be blank"
+  end
+
+  test "school email must be valid email" do
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: "cwhite72nv;oainse", major: "Psychology", minor: "CS", graduation_date: 2024/12/13)
+    assert_not student.save
+    assert_includes student.errors[:school_email], "is invalid"
+  end
+
+  test "must have graduation date" do
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: "cwhite72@msudenver.edu", major: "Psychology", minor: "CS", graduation_date: nil )
+    assert_not student.save
+    assert_includes student.errors[:graduation_date], "can't be blank"
+  end
+
+  test "create student without minor" do 
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: "cwhite72@msudenver.edu", major: "Psychology", minor: " ", graduation_date: 2024/12/13)
+    assert student.save
+  end
+
+  test "major must not be blank" do
+    student = Student.new(first_name: "Chase", last_name: "White", school_email: "cwhite72@msudenver.edu", major: " ", minor: "CS", graduation_date: 2024/12/13)
+    assert_not student.save
+    assert_includes student.errors[:major], "can't be blank"
+  end
 end
