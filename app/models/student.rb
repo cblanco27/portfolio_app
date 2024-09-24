96b98259b501f84fff14e675e81616b6959bd1f6
Chase White <chase@Chases-MacBook-Pro.local>
2024-09-23 21:20:49 -0600

    completed user story 3

--- app/models/student.rb
+++ app/models/student.rb
@@ -5,5 +5,6 @@ class Student < ApplicationRecord
   validates :major, presence: true
   validates :graduation_date, presence:true
   validates :school_email, format: { with: /\A[A-Za-z0-9+.-]+@[A-Za-z0-9.-]+\z/}
+  has_one_attached :profile_pic
 
 end
