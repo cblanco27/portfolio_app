db2c45c7eea22b9b0acbd206044c415d1b5c0b72
Chase White <chase@Chases-MacBook-Pro.local>
2024-10-09 17:39:07 -0600

    added initial dropdown functionality to major field of student creation

--- app/models/student.rb
+++ app/models/student.rb
@@ -7,4 +7,7 @@ class Student < ApplicationRecord
   validates :school_email, format: { with: /\A[A-Za-z0-9+.-]+@[A-Za-z0-9.-]+\z/}
   has_one_attached :profile_pic
 
+  VALID_MAJORS = ["Computer Engineering BS", "Computer Information Systems BS", "Computer Science BS", "Cybersecurity", "Data Science and Machine Learning Major"]
+
+  validates :major, inclusion: {in: VALID_MAJORS, message: "%{value} is not a valid major"}
 end
