9b7332119a85d8be0f322dc08a43d6bffb6bac65
Chase White <chase@Chases-MacBook-Pro.local>
2024-10-23 19:12:04 -0600

    removed school_email from model, view, and controller to get ready for devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_09_24_004340) do
+ActiveRecord::Schema[7.1].define(version: 2024_10_24_010641) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -42,13 +42,11 @@ ActiveRecord::Schema[7.1].define(version: 2024_09_24_004340) do
   create_table "students", force: :cascade do |t|
     t.string "first_name"
     t.string "last_name"
-    t.string "school_email"
     t.string "major"
     t.string "minor"
     t.date "graduation_date"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.index ["school_email"], name: "index_students_on_school_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
